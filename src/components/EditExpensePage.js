2e198707aa0fe1225874731b693635144d819573
Giedre Klimaite <giedre.klima@gmail.com>
2021-07-02 22:04:52 +0300

    query strings and URL params

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,5 +1,8 @@
 import React from "react";
 
-const EditExpensePage = () => <div>This is from EDIT Expense page</div>;
+const EditExpensePage = (props) => {
+  console.log(props);
+  return <div>Edit the Expense with id of {props.match.params.id}</div>;
+};
 
 export default EditExpensePage;
