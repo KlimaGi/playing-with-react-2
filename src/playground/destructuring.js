dccd24bcae8a4e125918dd052b824819dd85103e
Giedre Klimaite <giedre.klima@gmail.com>
2021-07-04 14:32:59 +0300

    array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,4 +1,6 @@
-// const person = {
+// Object destructuring
+
+//const person = {
 //   name: "Andrew",
 //   age: 31,
 //   location: {
@@ -14,14 +16,33 @@
 // const { city, temp: temperature } = person.location;
 // console.log(`It's ${temperature} in ${city}`);
 
-const book = {
-  title: "Ego is the Enemy",
-  author: "Ryan Holiday",
-  publisher: {
-    name: "Penguin",
-  },
-};
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holiday",
+//   publisher: {
+//     name: "Penguin",
+//   },
+// };
 
-const { name: publisherName = "Self-Published" } = book.publisher;
+// const { name: publisherName = "Self-Published" } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+//
+// Array destructuring
+//
+
+// const address = [
+//   "1299 Jupiter street",
+//   "Philadelphia",
+//   "Pennsylvania",
+//   "19324",
+// ];
+
+// const [, city, state = "New York"] = address;
+// console.log(`You are in ${state}`);
+
+const item = ["Coffee (hot)", "$2.00", "$2.50", "$2.75"];
+
+const [coffeeName, , medium] = item;
+console.log(`A medium ${coffeeName} costs ${medium}`);
