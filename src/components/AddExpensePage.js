8492b89d58f4d44820dbef6569e22e36cac68417
Giedre Klimaite <giedre.klima@gmail.com>
2021-07-07 20:32:34 +0300

    creating expense add/edit form

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import ExpenseForm from "./ExpenseForm";
 
-const AddExpensePage = () => <div>This is from Add Expense page</div>;
+const AddExpensePage = () => (
+  <div>
+    <h2>Add Expense</h2>
+    <ExpenseForm />
+  </div>
+);
 
 export default AddExpensePage;
