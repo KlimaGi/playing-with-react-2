47c95c25e9211c460d7c813be15dec0a28a1545f
Giedre Klimaite <giedre.klima@gmail.com>
2021-07-18 19:25:18 +0300

    wiring up edit expense

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -8,14 +8,19 @@ console.log(now.format("MMM Do, YYYY"));
 //console.log(now.format("YYYY-MM-DD HH:mm"));
 
 export default class ExpenseForm extends React.Component {
-  state = {
-    description: "",
-    note: "",
-    amount: "",
-    createdAt: moment(),
-    calendarFocused: false,
-    error: "",
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      description: props.expense ? props.expense.description : "",
+      note: props.ExpenseForm ? props.expense.note : "",
+      amount: props.expense ? (props.expense.amount / 100).toString() : "",
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+      calendarFocused: false,
+      error: "",
+    };
+  }
+
   onDescriptionChange = (e) => {
     const description = e.target.value;
     this.setState(() => ({ description }));
