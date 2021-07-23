8f2306a6b92eeb52c3848e2af040eb781e25455b
Giedre Klimaite <giedre.klima@gmail.com>
2021-07-23 14:19:30 +0300

    testing ExpenseListFilters: part 2

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,8 +3,6 @@ import moment from "moment";
 import { SingleDatePicker } from "react-dates";
 
 const now = moment();
-console.log(now.format("MMM Do, YYYY"));
-//console.log(now.format("YYYY-MM-DD HH:mm"));
 
 export default class ExpenseForm extends React.Component {
   constructor(props) {
