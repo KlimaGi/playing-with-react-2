9a926e78609a7969c39fdf90531bb62c1e3ed4ce
Giedre Klimaite <giedre.klima@gmail.com>
2021-07-25 18:49:07 +0300

    adding total selector, tests first

--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -5,14 +5,14 @@ export default [
     id: "1",
     description: "Gum",
     note: "",
-    amount: 196,
+    amount: 195,
     createdAt: 0,
   },
   {
     id: "2",
     description: "Rent",
     note: "",
-    amount: 109600,
+    amount: 109500,
     createdAt: moment(0).subtract(4, "days").valueOf(),
   },
   {
