0c63d7b5adad449b76fabcbf3375f2b87d036f4e
Giedre Klimaite <giedre.klima@gmail.com>
2021-07-29 23:57:10 +0300

    asynchronous redux actions

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,21 +1,45 @@
 import { v4 as uuidv4 } from "uuid";
+import database from "../firebase/firebase";
+//already doing
+// component calls action generator
+// action generator returns object
+// component dispatches object
+// redux store changes
+
+//better do
+// components calls action generator
+// action generator returns function
+// component dispatches function (?) add modules
+// functions runs (has the ability to dispatch other actions and do whatever it wants)
 
 // ADD_EXPENSE
-export const addExpense = ({
-  description = "",
-  note = "",
-  amount = 0,
-  createdAt = 0,
-} = {}) => ({
+export const addExpense = (expense) => ({
   type: "ADD_EXPENSE",
-  expense: {
-    id: uuidv4(),
-    description,
-    note,
-    amount,
-    createdAt,
-  },
+  expense,
 });
+
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
+    const {
+      description = "",
+      note = "",
+      amount = 0,
+      createdAt = 0,
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
+    database
+      .ref("expenses")
+      .push(expense)
+      .then((ref) => {
+        dispatch(
+          addExpense({
+            id: ref.key,
+            ...expense,
+          })
+        );
+      });
+  };
+};
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,129 +15,131 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-// child_removed
-database.ref("expenses").on("child_removed", (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-});
+export { firebase, database as default };
 
-//child_changed
-database.ref("expenses").on("child_changed", (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-});
-
-//child_added - gets called for new ones and for existing ones
-database.ref("expenses").on("child_added", (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-});
-
-// database.ref("expenses").on("value", (snapshot) => {
-//   const expenses = [];
-
-//   snapshot.forEach((childSnapshot) => {
-//     expenses.push({
-//       id: childSnapshot.key,
-//       ...childSnapshot.val(),
-//     });
-//   });
-
-//   console.log(expenses);
+// // child_removed
+// database.ref("expenses").on("child_removed", (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
 // });
 
-// database
-//   .ref("expenses")
-//   .once("value")
-//   .then((snapshot) => {
-//     const expenses = [];
+// //child_changed
+// database.ref("expenses").on("child_changed", (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
-//     snapshot.forEach((childSnapshot) => {
-//       expenses.push({
-//         id: childSnapshot.key,
-//         ...childSnapshot.val(),
-//       });
-//     });
+// //child_added - gets called for new ones and for existing ones
+// database.ref("expenses").on("child_added", (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
-//     console.log(expenses);
-//   });
+// // database.ref("expenses").on("value", (snapshot) => {
+// //   const expenses = [];
 
-database.ref("expenses").push({
-  description: "workation",
-  note: "",
-  amount: 10200,
-  createdAt: 2364234,
-});
+// //   snapshot.forEach((childSnapshot) => {
+// //     expenses.push({
+// //       id: childSnapshot.key,
+// //       ...childSnapshot.val(),
+// //     });
+// //   });
 
-// database.ref("expenses/-Mfd3h_h3krWy26HtMVQ").update({
-//   description: "present",
+// //   console.log(expenses);
+// // });
+
+// // database
+// //   .ref("expenses")
+// //   .once("value")
+// //   .then((snapshot) => {
+// //     const expenses = [];
+
+// //     snapshot.forEach((childSnapshot) => {
+// //       expenses.push({
+// //         id: childSnapshot.key,
+// //         ...childSnapshot.val(),
+// //       });
+// //     });
+
+// //     console.log(expenses);
+// //   });
+
+// database.ref("expenses").push({
+//   description: "workation",
 //   note: "",
-//   amount: 300,
+//   amount: 10200,
+//   createdAt: 2364234,
 // });
 
-// database.ref("notes/-Mfd-Mff9X_2pdiO6wRG").remove();
-// database.ref("notes/-Mfd-Mff9X_2pdiO6wRG").update({
-//   body: "Ride a bike",
-// });
+// // database.ref("expenses/-Mfd3h_h3krWy26HtMVQ").update({
+// //   description: "present",
+// //   note: "",
+// //   amount: 300,
+// // });
 
-// database.ref("notes").push({
-//   title: "Course Topics",
-//   body: "tdd, TypeScript, Node.js",
-// });
+// // database.ref("notes/-Mfd-Mff9X_2pdiO6wRG").remove();
+// // database.ref("notes/-Mfd-Mff9X_2pdiO6wRG").update({
+// //   body: "Ride a bike",
+// // });
 
-// database.ref().on(
-//   "value",
-//   (snapshot) => {
-//     const val = snapshot.val();
-//     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
-//   },
-//   (error) => {
-//     console.log("Error with data fetching", error);
-//   }
-// );
+// // database.ref("notes").push({
+// //   title: "Course Topics",
+// //   body: "tdd, TypeScript, Node.js",
+// // });
 
-// database
-//   .ref()
-//   .once("value")
-//   .then((snapshot) => {
-//     const val = snapshot.val();
-//     console.log(val);
-//   })
-//   .catch((error) => {
-//     console.log("Error fetching data", error);
-//   });
+// // database.ref().on(
+// //   "value",
+// //   (snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// //   },
+// //   (error) => {
+// //     console.log("Error with data fetching", error);
+// //   }
+// // );
 
-// database
-//   .ref()
-//   .set({
-//     name: "Giedre",
-//     age: 31,
-//     stressLevel: 6,
-//     job: {
-//       title: "Software developer",
-//       company: "Google",
-//     },
-//     location: {
-//       city: "Vilnius",
-//       country: "Lithuania",
-//     },
-//   })
-//   .then(() => {
-//     console.log("Data is saved!");
-//   })
-//   .catch((error) => {
-//     console.log("This failed.", error);
-//   });
+// // database
+// //   .ref()
+// //   .once("value")
+// //   .then((snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(val);
+// //   })
+// //   .catch((error) => {
+// //     console.log("Error fetching data", error);
+// //   });
 
-// database.ref().update({
-//   stressLevel: 9,
-//   "job/company": "IBM",
-//   "location/city": "Vilnius/remote",
-// });
+// // database
+// //   .ref()
+// //   .set({
+// //     name: "Giedre",
+// //     age: 31,
+// //     stressLevel: 6,
+// //     job: {
+// //       title: "Software developer",
+// //       company: "Google",
+// //     },
+// //     location: {
+// //       city: "Vilnius",
+// //       country: "Lithuania",
+// //     },
+// //   })
+// //   .then(() => {
+// //     console.log("Data is saved!");
+// //   })
+// //   .catch((error) => {
+// //     console.log("This failed.", error);
+// //   });
 
-// database
-//   .ref("isSingle")
-//   .remove()
-//   .then(() => {
-//     console.log("Remove succeeded.");
-//   })
-//   .catch((error) => {
-//     console.log("Remove failed: ", error);
-//   });
+// // database.ref().update({
+// //   stressLevel: 9,
+// //   "job/company": "IBM",
+// //   "location/city": "Vilnius/remote",
+// // });
+
+// // database
+// //   .ref("isSingle")
+// //   .remove()
+// //   .then(() => {
+// //     console.log("Remove succeeded.");
+// //   })
+// //   .catch((error) => {
+// //     console.log("Remove failed: ", error);
+// //   });
