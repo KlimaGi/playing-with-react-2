da430b920191e959fd4ad232825c204ca9e572ee
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-10 22:32:30 +0300

    error handling

--- webpack.config.js
+++ webpack.config.js
@@ -54,14 +54,20 @@ module.exports = (env) => {
         "process.env.FIREBASE_API_KEY": JSON.stringify(
           process.env.FIREBASE_API_KEY
         ),
-        "process.env.AUTH_DOMAIN": JSON.stringify(process.env.AUTH_DOMAIN),
-        "process.env.DATABASE_URL": JSON.stringify(process.env.DATABASE_URL),
-        "process.env.PROJECT_ID": JSON.stringify(process.env.PROJECT_ID),
-        "process.env.STORAGE_BUCKET": JSON.stringify(
-          process.env.STORAGE_BUCKET
+        "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
+          process.env.FIREBASE_AUTH_DOMAIN
         ),
-        "process.env.MESSAGING_SENDER_ID": JSON.stringify(
-          process.env.MESSAGING_SENDER_ID
+        "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
+          process.env.FIREBASE_DATABASE_URL
+        ),
+        "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
+          process.env.FIREBASE_PROJECT_ID
+        ),
+        "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
+          process.env.FIREBASE_STORAGE_BUCKET
+        ),
+        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
+          process.env.FIREBASE_MESSAGING_SENDER_ID
         ),
       }),
     ],
