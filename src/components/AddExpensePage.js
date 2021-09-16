24619d1098497291f941a84fbd0ff5b183db6dc7
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-16 16:07:26 +0300

    expense form style

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -11,8 +11,14 @@ export class AddExpensePage extends React.Component {
   render() {
     return (
       <div>
-        <h2>Add Expense</h2>
-        <ExpenseForm onSubmit={this.onSubmit} />
+        <div className="page-header">
+          <div className="content-container">
+            <h2 className="page-header__title">Add Expense</h2>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm onSubmit={this.onSubmit} />
+        </div>
       </div>
     );
   }
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -15,8 +15,17 @@ export class EditExpensePage extends React.Component {
   render() {
     return (
       <div>
-        <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
-        <button onClick={this.onRemove}>Remove</button>
+        <div className="page-header">
+          <div className="content-container">
+            <h2 className="page-header__title">Edit Expense</h2>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+          <button className="button button--secondary" onClick={this.onRemove}>
+            Remove Expense
+          </button>
+        </div>
       </div>
     );
   }
